249fb0a6b9f40104ed2fdf4c27b9d10d653a1631
John Arumemi-Ikhide <john.arumemi@gmail.com>
2025-03-10 11:35:41 +0000

    add debug logging
    I am noticing that an anchor test is taking > 10 seconds to run, even
    just for a single actual test. I am just placing some logs of data +
    timings to get a sense of where the slowdown is. I suspect it's the
    startup phase, but just need to confirm.

--- anchor/tests/votingdapp.spec.ts
+++ anchor/tests/votingdapp.spec.ts
@@ -24,17 +24,30 @@ describe('Voting', () => {
 
     it('Initialise Poll', async () => {
 
+        const description = "What is your favourte colour?"
+
+        let startTime = performance.now();
         // name must match the Program name. See Anchor.toml file for valid names.
         const context = await startAnchor("", [{ name: "votingdapp", programId: votingAddress }], []);
+        let duration = performance.now() - startTime;
 
+        console.log(`Created context in ${duration} milliseconds`)
+
+        startTime = performance.now();
         const provider = new BankrunProvider(context);
+        duration = performance.now() - startTime;
+        console.log(`Created provider in ${duration} milliseconds`)
 
+        startTime = performance.now();
         const votingProgram = new Program<Votingdapp>(
             IDL,
             provider,
         );
+        duration = performance.now() - startTime;
+        console.log(`Created voting_app in ${duration} milliseconds`)
 
         // represent a u65 using BN (Big Number)
+        startTime = performance.now();
         const tx = await votingProgram.methods.initialisePoll(
             new BN(1),
             new BN(0),
@@ -44,7 +57,23 @@ describe('Voting', () => {
             // Will call the initialisePoll method on the smart contract
             .rpc();
 
+        duration = performance.now() - startTime;
+        console.log(`poll initialised in ${duration} milliseconds`)
+
+        const [poll_address] = PublicKey.findProgramAddressSync(
+            [new BN(1).toArrayLike(Buffer, "le", 8)],
+            votingAddress
+        );
+
+        // fetch the poll account from this address
+        const poll = await votingProgram.account.poll.fetch(poll_address);
+        console.log(poll)
+
+        // get poll address from the program derived address
         console.log('Your transaction signature', tx);
+
+        expect(poll.pollId.toNumber()).toEqual(1)
+        expect(poll.pollStart.toNumber()).toBeLessThan(poll.pollEnd.toNumber())
     }, 15000) // 15 seconds
 
 })
